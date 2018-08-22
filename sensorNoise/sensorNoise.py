7f2567684e1c5fd3c9f72613c38248b80118a680
Ankur Kaul <ankurkaul17@gmail.com>
2018-08-22 02:12:31 -0400

    style: Add code comments inline

--- sensorNoise/sensorNoise.py
+++ sensorNoise/sensorNoise.py
@@ -1,6 +1,8 @@
 import numpy as np             
 import os
 import sys
+
+#change system directory to directory of the executing file
 os.chdir(os.path.dirname(sys.argv[0]))
 
 graph1 = np.genfromtxt('Graph1.txt', delimiter=',', dtype=None, encoding='utf8')
